24a87ec3c1777ae483945510c1db5a42a7f0ae44
Fellipe Philot <fpphilot@gmail.com>
2021-06-04 21:51:42 -0300

    Primeira versao do programa

---
+++ pac_update.py
@@ -0,0 +1,12 @@
+'''
+Primeira versão, ainda em teste.
+Automatização simples para atualização dos repositórios e programas instalados, no arch linux.
+Apenas necessário colocar a senha do sudo.
+
+'''
+
+print("<< 7777777777777777777 >> Atualização Automática Arch-Pac- Syyu << 7777777777777777777 >> ")
+
+import os  # Importação da biblioteca que interage com sistema operacional
+
+os.system("sudo pacman -Syyu ") #Inserção do comando no terminal
